84b80d5415523751f70986e5dacca5d91c3eeb06
IhanBender <ibbender@inf.ufpel.edu.br>
2018-12-01 10:24:44 -0200

    Encontradas coordenadas dos quadrados

--- Client/main.py
+++ Client/main.py
@@ -15,6 +15,117 @@ WHITE = (255,255,255)
 
 SQUARE_SIDE = 30
 
+
+initialX = 387 - SQUARE_SIDE
+xValues = [
+        initialX, initialX, initialX, initialX, initialX, initialX,     # Column with 6 squares
+        initialX - SQUARE_SIDE,
+        initialX - SQUARE_SIDE,
+        initialX - SQUARE_SIDE * 2,
+        initialX - SQUARE_SIDE * 3,
+        initialX - SQUARE_SIDE * 4,
+        initialX - SQUARE_SIDE * 5,
+        initialX - SQUARE_SIDE * 6,
+        initialX - SQUARE_SIDE * 6,
+        initialX - SQUARE_SIDE * 6,
+        initialX - SQUARE_SIDE * 5,
+        initialX - SQUARE_SIDE * 4,
+        initialX - SQUARE_SIDE * 3,
+        initialX - SQUARE_SIDE * 2,
+        initialX - SQUARE_SIDE,
+        initialX - SQUARE_SIDE,
+        initialX, initialX, initialX, initialX, initialX, initialX,     # Column with 6 squares
+        initialX + SQUARE_SIDE,
+        initialX + SQUARE_SIDE * 2,
+        initialX + SQUARE_SIDE * 2,
+        initialX + SQUARE_SIDE * 2,
+        initialX + SQUARE_SIDE * 2,
+        initialX + SQUARE_SIDE * 2,
+        initialX + SQUARE_SIDE * 2,
+        initialX + SQUARE_SIDE * 3,
+        initialX + SQUARE_SIDE * 3,
+        initialX + SQUARE_SIDE * 4,
+        initialX + SQUARE_SIDE * 5,
+        initialX + SQUARE_SIDE * 6,
+        initialX + SQUARE_SIDE * 7,
+        initialX + SQUARE_SIDE * 8,
+        initialX + SQUARE_SIDE * 8,
+        initialX + SQUARE_SIDE * 8,
+        initialX + SQUARE_SIDE * 7,
+        initialX + SQUARE_SIDE * 6,
+        initialX + SQUARE_SIDE * 5,
+        initialX + SQUARE_SIDE * 4,
+        initialX + SQUARE_SIDE * 3,
+        initialX + SQUARE_SIDE * 3,
+        initialX + SQUARE_SIDE * 2,
+        initialX + SQUARE_SIDE * 2,
+        initialX + SQUARE_SIDE * 2,
+        initialX + SQUARE_SIDE * 2,
+        initialX + SQUARE_SIDE * 2,
+        initialX + SQUARE_SIDE * 2,
+        initialX + SQUARE_SIDE
+]
+
+initialY = 600-75-SQUARE_SIDE
+yValues = [
+        initialY,
+        initialY - SQUARE_SIDE,
+        initialY - SQUARE_SIDE * 2,
+        initialY - SQUARE_SIDE * 3,
+        initialY - SQUARE_SIDE * 4,
+        initialY - SQUARE_SIDE * 5,
+        initialY - SQUARE_SIDE * 5,
+        initialY - SQUARE_SIDE * 6,
+        initialY - SQUARE_SIDE * 6,
+        initialY - SQUARE_SIDE * 6,
+        initialY - SQUARE_SIDE * 6,
+        initialY - SQUARE_SIDE * 6,
+        initialY - SQUARE_SIDE * 6,
+        initialY - SQUARE_SIDE * 7,
+        initialY - SQUARE_SIDE * 8,
+        initialY - SQUARE_SIDE * 8,
+        initialY - SQUARE_SIDE * 8,
+        initialY - SQUARE_SIDE * 8,
+        initialY - SQUARE_SIDE * 8,
+        initialY - SQUARE_SIDE * 8,
+        initialY - SQUARE_SIDE * 9,
+        initialY - SQUARE_SIDE * 9,
+        initialY - SQUARE_SIDE * 10,
+        initialY - SQUARE_SIDE * 11,
+        initialY - SQUARE_SIDE * 12,
+        initialY - SQUARE_SIDE * 13,
+        initialY - SQUARE_SIDE * 14,
+        initialY - SQUARE_SIDE * 14,
+        initialY - SQUARE_SIDE * 14,
+        initialY - SQUARE_SIDE * 13,
+        initialY - SQUARE_SIDE * 12,
+        initialY - SQUARE_SIDE * 11,
+        initialY - SQUARE_SIDE * 10,
+        initialY - SQUARE_SIDE * 9,
+        initialY - SQUARE_SIDE * 9,
+        initialY - SQUARE_SIDE * 8,
+        initialY - SQUARE_SIDE * 8,
+        initialY - SQUARE_SIDE * 8,
+        initialY - SQUARE_SIDE * 8,
+        initialY - SQUARE_SIDE * 8,
+        initialY - SQUARE_SIDE * 8,
+        initialY - SQUARE_SIDE * 7,
+        initialY - SQUARE_SIDE * 6,
+        initialY - SQUARE_SIDE * 6,
+        initialY - SQUARE_SIDE * 6,
+        initialY - SQUARE_SIDE * 6,
+        initialY - SQUARE_SIDE * 6,
+        initialY - SQUARE_SIDE * 6,
+        initialY - SQUARE_SIDE * 5,
+        initialY - SQUARE_SIDE * 5,
+        initialY - SQUARE_SIDE * 4,
+        initialY - SQUARE_SIDE * 3,
+        initialY - SQUARE_SIDE * 2,
+        initialY - SQUARE_SIDE,
+        initialY,
+        initialY
+]
+
 done = False
 
 def drawBoard():
@@ -22,10 +133,10 @@ def drawBoard():
     screen.fill([255, 255, 255])
     screen.blit(BACKGROUND_IMAGE, BACKGROUND_IMAGE.get_rect())
     # Board
-    pygame.draw.rect(screen, WHITE, pygame.Rect(150, 50, 500, 500))   
+    pygame.draw.rect(screen, WHITE, pygame.Rect(150, 50, 500, 500))
     pygame.draw.rect(screen, (BORDER_BLUE), [150, 50, 500, 500], 5)
     pygame.draw.rect(screen, (BORDER_BLUE), [160, 60, 480, 480], 2)
-    # Circles 
+    # Circles
     # Green Circle
     pygame.draw.circle(screen, GREEN, (250, 150), 500/8)
     pygame.draw.circle(screen, (BORDER_BLUE), (250, 150), 500/8, 5)
@@ -34,7 +145,7 @@ def drawBoard():
     pygame.draw.circle(screen, WHITE, (230, 170), 500/32)
     pygame.draw.circle(screen, WHITE, (270, 130), 500/32)
     # Blue Circle
-    pygame.draw.circle(screen, BLUE, (550, 150), 500/8) 
+    pygame.draw.circle(screen, BLUE, (550, 150), 500/8)
     pygame.draw.circle(screen, (BORDER_BLUE), (550, 150), 500/8, 5)
     pygame.draw.circle(screen, WHITE, (800-230, 130), 500/32)
     pygame.draw.circle(screen, WHITE, (800-270, 170), 500/32)
@@ -53,7 +164,7 @@ def drawBoard():
     pygame.draw.circle(screen, WHITE, (800-230, 600-130), 500/32)
     pygame.draw.circle(screen, WHITE, (800-270, 600-170), 500/32)
     pygame.draw.circle(screen, WHITE, (800-230, 600-170), 500/32)
-    pygame.draw.circle(screen, WHITE, (800-270, 600-130), 500/32) 
+    pygame.draw.circle(screen, WHITE, (800-270, 600-130), 500/32)
     # Central Area
     DOT1 = [175 + SQUARE_SIDE * 6 + 2, 75 + SQUARE_SIDE * 6]
     DOT2 = [175 + SQUARE_SIDE * 9 + 2, 75 + SQUARE_SIDE * 6]
@@ -63,7 +174,6 @@ def drawBoard():
 
     #pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(175+ SQUARE_SIDE*5+2, 75 + SQUARE_SIDE * 9, SQUARE_SIDE, SQUARE_SIDE), 2)   
     #pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(800-x-SQUARE_SIDE + 1, 75 + SQUARE_SIDE * 9, SQUARE_SIDE, SQUARE_SIDE), 2)   
-    
 
     pygame.draw.polygon(screen, BLUE, [DOT1, DOT2, CENTER])
     pygame.draw.polygon(screen, GREEN, [DOT1, DOT3, CENTER])
@@ -85,55 +195,28 @@ def drawBoard():
         y = y + SQUARE_SIDE
         x = x + SQUARE_SIDE
         # Blue
-        pygame.draw.rect(screen, BLUE, pygame.Rect(387, y, SQUARE_SIDE, SQUARE_SIDE))   
-        pygame.draw.rect(screen, WHITE, pygame.Rect(387, y, SQUARE_SIDE, SQUARE_SIDE), 2)   
-        pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(387 - SQUARE_SIDE, y, SQUARE_SIDE, SQUARE_SIDE), 2)   
-        pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(387 + SQUARE_SIDE, y, SQUARE_SIDE, SQUARE_SIDE), 2)   
+        pygame.draw.rect(screen, BLUE, pygame.Rect(387, y, SQUARE_SIDE, SQUARE_SIDE))
+        pygame.draw.rect(screen, WHITE, pygame.Rect(387, y, SQUARE_SIDE, SQUARE_SIDE), 2)
         # Red
-        pygame.draw.rect(screen, RED, pygame.Rect(387, 600-SQUARE_SIDE-y, SQUARE_SIDE, SQUARE_SIDE))   
-        pygame.draw.rect(screen, WHITE, pygame.Rect(387, 600-SQUARE_SIDE-y, SQUARE_SIDE, SQUARE_SIDE), 2)   
-        pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(387 - SQUARE_SIDE, 600-SQUARE_SIDE-y, SQUARE_SIDE, SQUARE_SIDE), 2)   
-        pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(387 + SQUARE_SIDE, 600-SQUARE_SIDE-y, SQUARE_SIDE, SQUARE_SIDE), 2)   
+        pygame.draw.rect(screen, RED, pygame.Rect(387, 600-SQUARE_SIDE-y, SQUARE_SIDE, SQUARE_SIDE))
+        pygame.draw.rect(screen, WHITE, pygame.Rect(387, 600-SQUARE_SIDE-y, SQUARE_SIDE, SQUARE_SIDE), 2)
         # Green
-        pygame.draw.rect(screen, GREEN, pygame.Rect(x+2, 75 + SQUARE_SIDE * 7, SQUARE_SIDE, SQUARE_SIDE))   
-        pygame.draw.rect(screen, WHITE, pygame.Rect(x+2, 75 + SQUARE_SIDE * 7, SQUARE_SIDE, SQUARE_SIDE), 2)   
-        pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(x+2, 75 + SQUARE_SIDE * 6, SQUARE_SIDE, SQUARE_SIDE), 2)   
-        pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(x+2, 75 + SQUARE_SIDE * 8, SQUARE_SIDE, SQUARE_SIDE), 2)   
+        pygame.draw.rect(screen, GREEN, pygame.Rect(x+2, 75 + SQUARE_SIDE * 7, SQUARE_SIDE, SQUARE_SIDE))
+        pygame.draw.rect(screen, WHITE, pygame.Rect(x+2, 75 + SQUARE_SIDE * 7, SQUARE_SIDE, SQUARE_SIDE), 2)
         # Yellow
-        pygame.draw.rect(screen, YELLOW, pygame.Rect(800-x-SQUARE_SIDE + 1, 75 + SQUARE_SIDE * 7, SQUARE_SIDE, SQUARE_SIDE))   
-        pygame.draw.rect(screen, WHITE, pygame.Rect(800-x-SQUARE_SIDE + 1, 75 + SQUARE_SIDE * 7, SQUARE_SIDE, SQUARE_SIDE), 2)   
-        pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(800-x-SQUARE_SIDE + 1, 75 + SQUARE_SIDE * 6, SQUARE_SIDE, SQUARE_SIDE), 2)   
-        pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(800-x-SQUARE_SIDE + 1, 75 + SQUARE_SIDE * 8, SQUARE_SIDE, SQUARE_SIDE), 2)  
-    # Blue
-    pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(387 - SQUARE_SIDE, 75, SQUARE_SIDE, SQUARE_SIDE), 2)   
-    pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(387, 75, SQUARE_SIDE, SQUARE_SIDE,), 2)   
-    pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(387 + SQUARE_SIDE, 75, SQUARE_SIDE, SQUARE_SIDE), 2)   
-    # Red
-    pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(387 - SQUARE_SIDE, 600-75-SQUARE_SIDE, SQUARE_SIDE, SQUARE_SIDE), 2)   
-    pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(387, 600-75-SQUARE_SIDE, SQUARE_SIDE, SQUARE_SIDE,), 2)   
-    pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(387 + SQUARE_SIDE, 600-75-SQUARE_SIDE, SQUARE_SIDE, SQUARE_SIDE), 2)   
-    # Green
-    pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(177, 75 + SQUARE_SIDE * 6, SQUARE_SIDE, SQUARE_SIDE), 2)   
-    pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(177, 75 + SQUARE_SIDE * 7, SQUARE_SIDE, SQUARE_SIDE,), 2)   
-    pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(177, 75 + SQUARE_SIDE * 8, SQUARE_SIDE, SQUARE_SIDE), 2) 
-    # Yellow
-    pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(800-175-SQUARE_SIDE+1, 75 + SQUARE_SIDE * 6, SQUARE_SIDE, SQUARE_SIDE), 2)   
-    pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(800-175-SQUARE_SIDE+1, 75 + SQUARE_SIDE * 7, SQUARE_SIDE, SQUARE_SIDE,), 2)   
-    pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(800-175-SQUARE_SIDE+1, 75 + SQUARE_SIDE * 8, SQUARE_SIDE, SQUARE_SIDE), 2) 
-    # Remaining Squares
-    pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(175+ SQUARE_SIDE*5+2, 75 + SQUARE_SIDE * 5, SQUARE_SIDE, SQUARE_SIDE), 2)   
-    pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(800-x-SQUARE_SIDE + 1, 75 + SQUARE_SIDE * 5, SQUARE_SIDE, SQUARE_SIDE), 2)   
-    pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(175+ SQUARE_SIDE*5+2, 75 + SQUARE_SIDE * 9, SQUARE_SIDE, SQUARE_SIDE), 2)   
-    pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(800-x-SQUARE_SIDE + 1, 75 + SQUARE_SIDE * 9, SQUARE_SIDE, SQUARE_SIDE), 2)   
-    
+        pygame.draw.rect(screen, YELLOW, pygame.Rect(800-x-SQUARE_SIDE + 1, 75 + SQUARE_SIDE * 7, SQUARE_SIDE, SQUARE_SIDE))
+        pygame.draw.rect(screen, WHITE, pygame.Rect(800-x-SQUARE_SIDE + 1, 75 + SQUARE_SIDE * 7, SQUARE_SIDE, SQUARE_SIDE), 2)
 
-
+    for i in range(len(xValues)):
+        pygame.draw.rect(screen, BORDER_BLUE, pygame.Rect(xValues[i], yValues[i], SQUARE_SIDE, SQUARE_SIDE), 2)
 
 while not done:
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                        done = True
-        
-        
-        drawBoard()
-        pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                done = True
+
+    drawBoard()
+    pygame.display.flip()
