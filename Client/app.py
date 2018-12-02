7ddc6d6d98b910c50983b35f39797b28f911bbc0
IhanBender <ibbender@inf.ufpel.edu.br>
2018-12-02 18:44:38 -0200

    Adicionados arquivos base de cliente e servidor

---
+++ Client/app.py
@@ -0,0 +1,13 @@
+import socket
+HOST = '181.220.54.142'     # Endereco IP do Servidor
+PORT = 5000            # Porta que o Servidor esta
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+dest = (HOST, PORT)
+print ("Trying to connect")
+tcp.connect(dest)
+print 'Para sair use CTRL+X\n'
+msg = raw_input()
+while msg <> '\x18':
+    tcp.send (msg)
+    msg = raw_input()
+tcp.close()
